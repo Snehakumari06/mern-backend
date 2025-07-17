0bd1a19d23d9cabb39e9f197d2f62cd5d7fcf4fa
Snehakumari06 <snehasingh5754@gmail.com>
2025-07-17 10:14:42 +0530

    update

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ app.use(express.json());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-  app.listen(8080, () => {
-    console.log("Server started");
-  });
-});
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
 
-  // mongoose
-  // .connect(`mongodb+srv://${dbuser}:${dbpass}@sneha.tbgpj9n.mongodb.net/merndb?retryWrites=true&w=majority&appName=Sneha`)
+  mongoose
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@sneha.tbgpj9n.mongodb.net/merndb?retryWrites=true&w=majority&appName=Sneha`)
   
-  // .then(() => {
-  //   app.listen(8080, () => {
-  //     console.log("Server started");
-  //   });
-  // });
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
